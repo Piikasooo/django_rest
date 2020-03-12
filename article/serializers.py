521e8852bc22d8a5753416aad639b578f73c45d1
Your Name <artger@ukr.net>
2020-03-12 11:17:43 +0200

    Rewrite CRUD api with using GenericAPIview

--- article/serializers.py
+++ article/serializers.py
@@ -2,7 +2,11 @@ from rest_framework import serializers
 from .models import Article
 
 
-class ArticleSerializer(serializers.Serializer):
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ('id', 'title', 'description', 'body', 'author_id')
+
     title = serializers.CharField(max_length=120)
     description = serializers.CharField()
     body = serializers.CharField()
